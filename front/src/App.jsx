08e537571039ef2a2f72d1a6bdd93c38e6897e37
Anushkap-lab <anushkapatil560@gmail.com>
2026-04-24 16:25:20 +0530

    styling

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,5 +1,10 @@
 import MahatmeChatbot from "./chatbot.jsx";
 
 export default function App() {
-  return <MahatmeChatbot />;
+  return (
+    <main className="simple-home">
+      <h1 className="simple-home__title">Mahatme Eye Hospital</h1>
+      <MahatmeChatbot />
+    </main>
+  );
 }
